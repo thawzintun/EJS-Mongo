f094e806bbf60ebf7bffdd04350d1adf00b5031b
Thaw Zin Tun <digitalandit@proximityfinance.org>
2023-12-12 22:18:00 +0630

    cookies

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("views", "views");
 
 const postRoute = require("./routes/post");
 const adminRoute = require("./routes/admin");
+const authRoute = require("./routes/auth");
 const User = require("./models/user");
 dotenv = require("dotenv").config();
 
@@ -16,14 +17,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    User.findById("653020a2da6e3155a9796730").then(user => {
+    User.findById("653020a2da6e3155a9796730").then((user) => {
         req.user = user;
-        next()
-    } );
+        next();
+    });
 });
 
 app.use(postRoute);
 app.use("/admin", adminRoute);
+app.use(authRoute);
 
 mongoose
     .connect(process.env.MONGODB_URL)
